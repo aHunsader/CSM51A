18af668cfe528f2b888d5a409f40a457bd9d6440
aHunsader <alex@hunsader.com>
2018-04-12 13:49:05 -0700

    rounded and shit

--- lab1_t12/int_to_float.v
+++ lab1_t12/int_to_float.v
@@ -18,42 +18,6 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module priority_encoder(
-	input wire [11:0] IN,
-	output reg [3:0] OUT
-	);
-	
-	always @* begin
-		OUT = 0; // default value
-		if (IN[11]) OUT = 11;
-			
-		else if (IN[10]) OUT = 10;
-			
-		else if (IN[9]) OUT = 9;
-			
-		else if (IN[8]) OUT = 8;
-			
-		else if (IN[7]) OUT = 7;
-			
-		else if (IN[6]) OUT = 6;
-			
-		else if (IN[5]) OUT = 5;
-			
-		else if (IN[4]) OUT = 4;
-			
-		else if (IN[3]) OUT = 3;
-			
-		else if (IN[2]) OUT = 2;
-
-		else if (IN[1]) OUT = 1;
-			
-		else if (IN[0]) OUT = 0;
-		
-		else;
-  end
-  	
-endmodule
-
 module int_to_float(
     input [11:0] D,
     output S,
@@ -62,31 +26,94 @@ module int_to_float(
     );
 	 
 	 wire [11:0] mag;
-	 wire [3:0] leading_one;
 	 //wire first_bit;
-	 reg [3:0] first_bits = 0;
-	 assign S = D[11];
+	 wire [3:0] first_bit;
+	 wire [3:0] f_temp;
+	 wire round_bit;
+	 wire [2:0] exp;
+	
+	 sign sign1(.D(D), .S(S));
 	 
 	 assign mag[11:0] = (S) ? (~D + 1) : D;
-
-	priority_encoder p_encoder ( .IN(mag), .OUT(leading_one) );
+	 
+	 priority_encoder p1(.IN(mag), .OUT(first_bit));
+	 
+	 get_magnitude gm1(.enc(first_bit), .mag(mag), .ans(f_temp), .round_bit(round_bit));
+	 get_exp e1(.first_bit(first_bit), .exp(exp));
+	 
+	 round r1(.f_temp(f_temp), .exp(exp), .round_bit(round_bit), .F(F), .E(E));
+	 
+	 
 	
-	always @ (leading_one) begin
-		if (leading_one < 3)
-			first_bits[3:0] <= D[3:0];
-		else
-			first_bits[3:0] <= D[leading_one -: 4];
+endmodule
+	
+//get sign 
+module sign(input [11:0] D, output S);
+	assign S = D[11];
+endmodule
+
+//get magnitude value
+module get_magnitude(input [3:0] enc, input [11:0] mag, output reg [3:0] ans, output reg round_bit);
+	always @ (enc, mag) begin
+		if (enc <= 3) begin
+			ans[3:0] <= mag[3:0];
+			round_bit <= 0;
+		end
+		else begin
+			ans[3:0] <= mag[enc -: 4];
+			round_bit <= mag[enc-4];
+		end
+	end
+endmodule
+
+//get exponent value
+module get_exp(input [3:0] first_bit, output reg [2:0] exp);
+	always @ (first_bit) begin
+	if (first_bit <= 3)
+		exp[2:0] <= 0;
+	else
+		exp[2:0] <= first_bit[3:0] - 3;
 	end
 
-//	 always @ (mag) begin
-//		leading_one[3:0] = leading_one[3:0];//mag[leading_one-3 +: 3];
-//	 end
-	 
-	 assign F[3:0] = first_bits[3:0];
-	 assign E[2:0] = 0;
-	 
-	always @ (mag) begin
+endmodule
+
+module round(input [3:0] f_temp, input [2:0] exp, input round_bit, output [3:0] F, output [2:0] E);
+assign F = round_bit ? 4'b1000 : f_temp;
+assign E = ((exp >= 7) && round_bit) ? 7 : exp;
+endmodule
 	
-	end
-	 
+//get position of leading 1
+module priority_encoder(
+	input wire [11:0] IN,
+	output reg [3:0] OUT
+	);
+	
+	always @* begin
+		if (IN[11]) OUT <= 11;
+			
+		else if (IN[10]) OUT <= 10;
+			
+		else if (IN[9]) OUT <= 9;
+			
+		else if (IN[8]) OUT <= 8;
+			
+		else if (IN[7]) OUT <= 7;
+			
+		else if (IN[6]) OUT <= 6;
+			
+		else if (IN[5]) OUT <= 5;
+			
+		else if (IN[4]) OUT <= 4;
+			
+		else if (IN[3]) OUT <= 3;
+			
+		else if (IN[2]) OUT <= 2;
+
+		else if (IN[1]) OUT <= 1;
+			
+		else if (IN[0]) OUT <= 0;
+		
+		else OUT <= 0;
+  end
+  	
 endmodule
